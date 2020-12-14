02298e9ee24b75f998791e0b613b5a483390f02b
David Vulakh <david@vulakh.us>
2020-12-14 13:03:18 -0500

    Add splicing recovered genes to original topology

---
+++ analysis/shrink_genome.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+#####################################################################
+# Reads a pedigree from STDIN and outputs a pedigree with identical
+# structure of parent-child relationships but genomes shortened to
+# the command-line argument
+#####################################################################
+
+use strict;
+use warnings;
+use v5.16;
+
+my $LEN = shift;
+for (<STDIN>) {
+	chomp;
+	if (/^(.*-i.*)-g\s+(\d+)\s+([^\-]*)(.*)$/) {
+		my @gen = grep {$_} split /\s+/, $3;
+		print "$1-g $LEN @gen[0..$LEN-1]$4\n";
+	} elsif (/^(.*)-B\s+(\d+)(.*)$/) { print "$1-B $LEN$3\n"; }
+	else { print "$_\n"; }
+}
---
+++ analysis/splice_recovered.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+#####################################################################
+# Reads an original and reconstructed poisson pedigree from STDIN
+# and reads the treediff data file given as an argument. Prints a
+# pedigree with topology identical to the original pedigree, in
+# which the genome of each couple is the genome of its image in the
+# reconstructed pedigree (or all 0s if the couple was not recovered)
+#####################################################################
+
+use strict;
+use warnings;
+use v5.16;
+use List::Util 'max';
+use experimental 'smartmatch';
+
+### PATH TO RECGEN ###
+my $PATH_TO_RECGEN = @ARGV > 1 ? pop @ARGV : '.';
+
+### PEDIGREE INFORMATION ###
+# Bijection
+my %O_TO_R;
+# Lists of recovered genes for each couple (pair)
+my %R_TO_GEN;
+# Original pedigree string
+my $PED;
+
+### READ PEDIGREES ###
+my $SW = 0;
+for (<STDIN>) {
+	chomp;
+	# Switch to reading reconstructed pedigree on tilde
+	$SW = 1 if /~/;
+	# Store original pedigree
+	if ($SW == 0) {
+		$PED .= "$_\n";
+	}
+	# Read genome for recovered pedigree
+	elsif (/^i.*-c\s+(\d+).*-g\s+(\d+)\s+([\d\s]+)/) {
+		push @{$R_TO_GEN{$1}}, [ split /\s+/, $3 ];
+	}
+}
+
+### READ BIJECTION ###
+for (<>) { $O_TO_R{$1} = $2 if /bijection\s+\((\d+)o[^\d]*(\d+)r\)/; }
+
+### SPLICE GENOMES ONTO ORIGINAL ###
+for (split /\n/, $PED) {
+	if (/^(i.*-c\s+(\d+).*-g\s+(\d+)\s+)([\d\s]+)$/) {
+		my $l = $1; my $c = $2; my $ng = $3; my @g;
+		if (exists $O_TO_R{$c} && exists $R_TO_GEN{$O_TO_R{$c}} && @{$R_TO_GEN{$O_TO_R{$c}}} > 0) {
+			@g = @{pop @{$R_TO_GEN{$O_TO_R{$c}}}};
+		} else {
+			@g = (0) x $ng;
+		}
+		print $l.(join ' ', @g)."\n";
+	} else { print "$_\n"; }
+}
